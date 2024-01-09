f73111693d81666d00537736488c09b546d32383
rusilee <ildarelmeev@yandex>
2024-01-09 23:22:28 +0300

    add migrations

--- betting-backend/src/main/java/ru/itmo/betting_backend/api/BettingApiService.java
+++ betting-backend/src/main/java/ru/itmo/betting_backend/api/BettingApiService.java
@@ -2,12 +2,12 @@ package ru.itmo.betting_backend.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import ru.itmo.generated.api.PingApiDelegate;
-import ru.itmo.generated.model.PingResponse;
+import org.springframework.stereotype.Component;
+import ru.itmo.betting_backend.model.PingResponse;
 
-@Service
+@Component
 public class BettingApiService implements PingApiDelegate {
+
     @Override
     public ResponseEntity<PingResponse> ping() {
         return new ResponseEntity<>(
